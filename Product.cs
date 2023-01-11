6577ac5a1cb5cda676ac7eb51f914b07ae146546
emerson143 <emerson.143@hotmail.com>
2023-01-10 22:45:59 -0300

    conexao

--- Product.cs
+++ Product.cs
@@ -2,13 +2,9 @@ public class Product{
     public int Id { get; set;}   
     public string Code { get; set; }
     public string Name { get; set; }
-    
     public string Description { get; set;}
-    
     public int CategoryId {get; set;}
-
     public Category Category{ get; set;}
-    
     public List<Tag> tag { get; set;}
 
 }
