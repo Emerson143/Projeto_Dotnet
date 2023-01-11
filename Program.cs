a1c47fa3ef3d95bc476803d7794e67111b6005a5
emerson143 <emerson.143@hotmail.com>
2023-01-10 22:15:49 -0300

    separando as clases

--- Program.cs
+++ Program.cs
@@ -2,18 +2,15 @@ using System.Collections.Immutable;
 using System.Reflection.Emit;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>();
-
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServe"]);
 
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
-
 
 app.MapPost("/products", (Product product) => {
 
@@ -22,7 +19,6 @@ return Results.Created("/products" +product.Code, product.Code);
 
 });
 
-
 //api.app.com/user/{code}
 app.MapGet("/products/{code}", ([FromRoute] string code ) =>{
        var product = ProductRepository.GetBy(code);
@@ -30,25 +26,20 @@ app.MapGet("/products/{code}", ([FromRoute] string code ) =>{
               return Results.Ok(product);
 
        return Results.NotFound();
-
 });
 
 
 app.MapPut("/products", (Product product) => {
-
        var productSaved = ProductRepository.GetBy(product.Code);
        productSaved.Name = product.Name;
        return Results.Ok();
-
 });
 
 app.MapDelete("/products/{code}", ([FromRoute] string code ) => {
-
        var productSaved = ProductRepository.GetBy(code);
        ProductRepository.Remove(productSaved);
        return Results.Ok();
 });
-
 
 
 app.MapGet("/configuration/database", (IConfiguration configuration) => {
@@ -57,83 +48,4 @@ app.MapGet("/configuration/database", (IConfiguration configuration) => {
 
 });
 
-
 app.Run();
-
-public static class  ProductRepository{
-
-       public static List<Product > Products { get; set;} = Products = new List<Product>();
-
-       public static void Init(IConfiguration configuration){
-              var products = configuration.GetSection("Products").Get<List<Product>>();
-              Products = products;
-       }
-       public static void Add(Product product){
-              Products.Add(product);
-
-       }
-
-       public static Product GetBy(string code){
-              return Products.FirstOrDefault(p => p.Code == code);
-       }
-
-
-       public static void Remove( Product product){
-               Products.Remove(product);
-       }
-}
-
-public class Category {
-
-       public int Id { get; set;}
-
-       public string Name {get; set;}
-}
-       
-
-public class Tag{
-
-       public  int Id { get; set; }
-
-       public string Name { get; set; }
-
-       public int ProductId { get; set;}
-
-}
-
-public class Product{
-
-    public int Id { get; set;}   
-    
-    public string Code { get; set; }
-    
-    public string Name { get; set; }
-    
-    public string Description { get; set;}
-    
-    public int CategoryId {get; set;}
-
-    public Category Category{ get; set;}
-    
-    public List<Tag> tag { get; set;}
-
-}
-
-public class ApplicationDbContext : DbContext{
-
-      public DbSet<Product> Products { get; set; } 
-
-       protected override void OnModelCreating(ModelBuilder builder){
-
-              builder.Entity<Product>()
-              .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
-              builder.Entity<Product>()
-              .Property(p => p.Name).HasMaxLength(120).IsRequired();
-              builder.Entity<Product>()
-              .Property(p => p.Code).HasMaxLength(20).IsRequired();
-       }
-
-
-      protected override void OnConfiguring(DbContextOptionsBuilder options)
-      => options.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@Sql2022;MultipleActiveResultSets=true;Encrypt=Yes;TrustServerCertificate=Yes");
-}
